ab8450c8b46d1540eec6de18cbe00bbac3703353
EvgeniiKosarev <evgenii.kosarev@yahoo.co.uk>
2020-03-03 13:50:34 +0100

    final version

--- main.py
+++ main.py
@@ -7,27 +7,29 @@ def polindrome(size):
     expressed as the multiple of 2 numbers with number of digits = size in each
 
     '''
-
+    if isinstance(size, int) and size>0:
     
-    x = (10**size) - 1 # largest size-digit
-    z = 10**(size-1)
-    
-    all_polindromes = [] # smallest size-digit number
-    
-    for i in (a*b for a in range(z,x) for b in range(a, x)):
-        number = list(str(i))
+        x = (10**size) - 1 # largest size-digit
+        z = 10**(size-1)   # smallest size-digit number
         
-        first_half = number[:size]
-        second_half = number[size:]
-        second_half.reverse()
-
-        if first_half == second_half:
+        all_polindromes = [] 
+        
+        for i in (a*b for a in range(z,x) for b in range(a, x)):
+            number = list(str(i))
             
-            all_polindromes.append(i)
-    
-    all_polindromes.sort()
+            first_half = number[:size]
+            second_half = number[size:]
+            second_half.reverse()
 
-    if len(all_polindromes)!=0:
-        return all_polindromes[-1]
+            if first_half == second_half:
+                
+                all_polindromes.append(i)
+        
+        all_polindromes.sort()
+
+        if len(all_polindromes)!=0:
+            return all_polindromes[-1]
+        else:
+            return "no polindromes found"
     else:
-        return "no polindromes found"
+        raise ValueError
